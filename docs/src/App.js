2368b63cd52b8465a11b3c7be7d81954e89c4c9e
Matteo Manzinello <matteo@manzinello.com>
2018-04-08 21:29:51 +0200

    Aggiunti i componenti per la ricerca

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch/dom';
+
+const App = () => (
+  <InstantSearch
+    appId="latency"
+    apiKey="3d9875e51fbd20c7754e65422f7ce5e1"
+    indexName="bestbuy"
+  >
+        <SearchBox />
+
+    <Hits/>
+  </InstantSearch>
+);
 
 export default App;
